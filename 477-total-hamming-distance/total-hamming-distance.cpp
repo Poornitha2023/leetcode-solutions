3e56a70741671700ddcd5ee002747782dbc71d4f
Poornitha2023 <24B11CS500@adityauniversity.in>
2026-08-11 13:59:02 +0530

    Time: 1076 ms (7.47%) | Memory: 22.8 MB (81.93%) - LeetSync

---
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+
+            int a=nums[i]^nums[j];
+            sum+=__builtin_popcount(a);
+            }
+        }
+        return sum;
+    }
+};
