29c451426926ec6b0f387a30055c37f5d7be41cb
Poornitha Yanamala <24B11CS500@adityauniversity.in>
2026-09-22 16:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 24.7 MB (5.75%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -0,0 +1,100 @@
+class MyCircularDeque {
+public:
+    vector<int>arr;
+    int size;
+    int front;
+    int rear;
+    int capacity;
+    MyCircularDeque(int k) {
+        size=0;
+        capacity=k;
+        front=0;
+        rear=capacity-1;
+        arr.resize(capacity);
+        
+    }
+    
+    bool insertFront(int value) {
+        if(isFull()){
+            return false;
+        }if(isEmpty()){
+            front=0;
+            rear=0;
+        }else{
+      
+       front= (front-1+capacity) % capacity;
+        }
+          arr[front] = value;
+          size++;
+          return true;
+    }
+    
+    bool insertLast(int value) {
+        if(isFull()){ return false;}
+
+        if(isEmpty()){
+            front=0;
+            rear=0;
+        }else{
+             rear=(rear+1) % capacity;
+        }
+       arr[rear]=value;
+        size++;
+         return true;
+        
+    }
+    
+    bool deleteFront() {
+        if(isEmpty()){
+            return false;
+        }
+        front=(front+1)%capacity;
+        size--;
+        return true;
+
+    }
+    
+    bool deleteLast() {
+       if(isEmpty()){
+        return false;
+       }
+       rear=(rear-1+capacity)%capacity;
+       size--;
+       return true;
+    }
+    
+    int getFront() {
+        if(size==0){
+            return -1;
+        }
+        return arr[front];
+    }
+    
+    int getRear() {
+         if(isEmpty()){
+            return -1;
+        }
+        return arr[rear];
+    }
+    
+    bool isEmpty() {
+        return size==0;
+    }
+    
+    bool isFull() {
+        return size==capacity;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
