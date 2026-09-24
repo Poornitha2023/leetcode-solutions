47b5886683126d8a63f7f7e61c1282b3295a4d22
Poornitha Yanamala <24B11CS500@adityauniversity.in>
2026-09-24 21:27:21 +0530

    Time: 2 ms (9.57%) | Memory: 30.9 MB (81.66%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
-       
         for(int i=0;i<nums.size();i++){
             int n=nums[i];
-           int sum=0;
+            int sum=0;
             while(n>0){
-                sum+=n%10;
+                int a=n%10;
+                sum+=a;
                 n=n/10;
             }
             if(sum==i){
                 return i;
             }
-            }
-        
-        
+        }
         return -1;
     }
 };
